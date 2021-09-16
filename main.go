6dc26c8dce70ad1ad667fee497e6eac358b6f7de
todayIsHoho <wnsgh5049@naver.com>
2021-09-16 14:42:38 +0900

    URL CHECKER: Slow URLChecker

--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/junhoKim/learngo/mydict"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+var errRequestFailed = errors.New("Request Failed")
 
-	if err != nil {
-		fmt.Println(err)
+func main() {
+
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	errUpdate := dictionary.Update(word, "hihi")
-	dictionary.Delete(word)
-	hello, _ := dictionary.Search(word)
-	fmt.Println("errUpdate", errUpdate)
-	fmt.Println("found", word, "definition", hello)
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	// fmt.Println("resp:", resp)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
