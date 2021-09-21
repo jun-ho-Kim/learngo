ada5b93674f9e21d2977ddc1b6ec7297e9052f9c
todayIsHoho <wnsgh5049@naver.com>
2021-09-21 10:28:27 +0900

    WEB SERVER WITH Echo - Echo Server && JOB File download

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "github.com/junhoKim/learngo/scrapper"
+import (
+	"os"
+	"strings"
+
+	"github.com/junhoKim/learngo/scrapper"
+	"github.com/labstack/echo"
+)
+
+const flleName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScape(c echo.Context) error {
+	defer os.Remove("jobs.csv")
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment("jobs.csv", "job.csv")
+}
 
 func main() {
-	scrapper.Scrape("python")
-
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
